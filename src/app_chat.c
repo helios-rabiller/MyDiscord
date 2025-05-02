2210dd872b8481699ab2888df8a1004ac595cff8
lucas-de-souza-pereira <lucas.de-souza-pereira@laplateforme.io>
2025-05-02 11:30:01 +0200

    del show alert

--- src/app_chat.c
+++ src/app_chat.c
@@ -24,27 +24,6 @@ typedef struct {
     char selected_channel[128];
 } ChatContext;
 
-gboolean alert_create_channel(gpointer data, const char *message) {
-    ChatContext *ctx = (ChatContext *)data;
-
-    GtkWidget *dialog = gtk_dialog_new();
-    gtk_window_set_title(GTK_WINDOW(dialog), "Erreur");
-    gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
-    gtk_window_set_transient_for(GTK_WINDOW(dialog), GTK_WINDOW(gtk_widget_get_root(ctx->chat_label)));
-
-    GtkWidget *content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
-    GtkWidget *label = gtk_label_new(message);
-    gtk_box_append(GTK_BOX(content_area), label);
-
-    GtkWidget *ok_button = gtk_button_new_with_label("OK");
-    gtk_dialog_add_action_widget(GTK_DIALOG(dialog), ok_button, GTK_RESPONSE_OK);
-
-    g_signal_connect_swapped(ok_button, "clicked", G_CALLBACK(gtk_window_destroy), dialog);
-
-    gtk_window_present(GTK_WINDOW(dialog));
-    return FALSE; 
-}
-
 
 static gpointer listen_for_messages(gpointer user_data) {
 
